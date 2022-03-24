49c71d7b7664a7b5920a64a1600ad9ed2f4e5c90
RamiresMatias <ramiresmatias20000@gmail.com>
2022-03-23 21:10:16 -0300

    Add replacec loop with pipeline

---
+++ Capitulo_8_movendo_recursos/replace_loop_with_pipeline.js
@@ -0,0 +1,36 @@
+// Substituir laço por pipeline
+
+// Antes
+
+function acquireData(input) {
+    const lines = input.split('\n')
+    let firstLine = true
+    const result = []
+    for(const line of lines) {
+        if(firstLine) {
+            firstLine = false
+            continue
+        }
+        if(line.trim() === '') continue
+        const record = line.split(',')
+        if(record[1].trim() === 'India') {
+            result.push({city: record[0].trim(), phone: record[2].trim()})
+        }
+    }
+
+    return result
+}
+
+// Depois
+
+function acquireData(input) {
+    const lines = input.split('\n')
+    const result = lines
+        .slice(1)
+        .filter(line => line.trim() !== '')
+        .map(line => line.split(','))
+        .filter(record => record[1].trim() === 'India')
+        .map(record => ({city: record[0].trim(), phone: record[2].trim()}))
+
+    return result
+}
